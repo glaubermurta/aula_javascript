b28e0fc25ee4f0192e684a2789673e7e7c44fd03
Glauber Martins Murta <glaubermurta@hotmail.com>
2021-02-07 15:14:50 -0300

    Treinando o código

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+/*var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert (d.getMinutes());
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+
+/*
+var idade = prompt("Qual sua idade");
+if (idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de Idade");
+}
+*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].cor);
+*/
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+/*var nome= "Glauber Murta";
+var idade = 36;
+var idade2 = 10;
+var frase = "Brasil é o melhor time do mundo";
+//alert("Bem vindo " + nome);
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome)
+console.log(frase.toLocaleUpperCase);
+*/
+
+
